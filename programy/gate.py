0a2750215fa2935a503937114ed7ab34640eb033
Stefan <email>
2019-06-13 19:54:55 +0200

    brama: silnik do lasera

--- programy/gate.py
+++ programy/gate.py
@@ -9,12 +9,55 @@ import time
 
 light_sensor = ColorSensor(INPUT_1)
 touch_sensor = TouchSensor(INPUT_2)
+laser_motor = MediumMotor(OUTPUT_A)
+flag_motor = LargeMotor(OUTPUT_B)
+
 THRESHOLD = 40
 
 sound = Sound()
 
+
+
+def laser_on():
+    print("ENABLING LASER!")
+    # sound.speak("Enabling laser!")
+
+    laser_motor.on(10)
+
+    while light_sensor.ambient_light_intensity < THRESHOLD:
+        pass
+    
+    laser_motor.off()
+
+    laser_motor.on_for_degrees(10,50)
+        
+
+def laser_off():
+    print("DISABLING LASER!")
+    # sound.speak("Disabling laser!")
+
+    laser_motor.on(-10)
+
+    while light_sensor.ambient_light_intensity > THRESHOLD:
+        pass
+    
+    laser_motor.off()
+
+    laser_motor.on_for_degrees(-10,50)
+
+
+def flag_high():
+    flag_motor.on_for_degrees(30, -180)
+
+def flag_low():
+    flag_motor.on_for_degrees(30, 180)
+
+
 while True:
+    
     touch_sensor.wait_for_bump()
+    laser_on()
+    flag_high()
     print("Waiting for START!")
     sound.speak("Waiting for start!")
     print(light_sensor.ambient_light_intensity)
@@ -30,12 +73,15 @@ while True:
 
     while light_sensor.ambient_light_intensity > THRESHOLD and not touch_sensor.is_pressed:
         pass
-
+    
+    
     elapsed_time = time.time()-start_time
     elapsed_time_message = "Your time: {:.2f} seconds".format(elapsed_time)
     print(elapsed_time_message)
     sound.speak(elapsed_time_message)
+    laser_off()
+
+    flag_low()
 
     # poczekaj 2 sekundy pomiedz odczytami
-    time.sleep(2)
-
+    #time.sleep(2)
